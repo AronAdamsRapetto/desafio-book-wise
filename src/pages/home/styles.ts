6057c152f9871468bd1d2150b9af30a580f1037e
Aron Adams <aronrapetto@hotmail.com>
2023-08-16 11:27:29 -0300

    style: responsive home

--- src/pages/home/styles.ts
+++ src/pages/home/styles.ts
@@ -2,14 +2,25 @@ import { styled } from '@/styles/stitches.config'
 
 export const Container = styled('div', {
   display: 'grid',
-  gridTemplateColumns: '35% 65%',
+  gridTemplateColumns: '40% 60%',
   height: '100vh',
+
+  '@media(max-width: 768px)': {
+    display: 'flex',
+    flexDirection: 'column',
+  },
 })
 
 export const RigthSide = styled('div', {
   maxHeight: '100vh',
-  padding: '1.25rem',
+  padding: '$5',
   position: 'relative',
+
+  '@media(max-width: 768px)': {
+    height: '40%',
+    width: '100%',
+    padding: '$3',
+  },
 })
 
 export const InnerContainer = styled('div', {
@@ -26,12 +37,19 @@ export const LeftSide = styled('div', {
   display: 'flex',
   justifyContent: 'center',
   alignItems: 'center',
+
+  '@media(max-width: 768px)': {
+    width: '100%',
+    alignItems: 'flex-start',
+    marginTop: '$6',
+  },
 })
 
 export const MenuLogin = styled('div', {
   display: 'flex',
   flexDirection: 'column',
   width: '23.25rem',
+  margin: '$5',
   fontFamily: '$default',
 
   '> h3': {
