43e6009d768e34bb5bcb083adf3d490650cd4387
Aron Adams <aronrapetto@hotmail.com>
2023-08-16 17:42:29 -0300

    feat: finish home page login

--- src/components/LoginButton/index.tsx
+++ src/components/LoginButton/index.tsx
@@ -1,5 +1,6 @@
 import Image from 'next/image'
 import { signIn } from 'next-auth/react'
+import { useRouter } from 'next/router'
 
 import googleLogo from '../../assets/google_logo_1.png'
 import githubLogo from '../../assets/github_logo.svg'
@@ -11,6 +12,8 @@ interface LoginButtonProps {
 }
 
 export default function LoginButton({ variant }: LoginButtonProps) {
+  const router = useRouter()
+
   if (variant === 'google') {
     return (
       <ConnectButton onClick={() => signIn('google')}>
@@ -27,7 +30,7 @@ export default function LoginButton({ variant }: LoginButtonProps) {
     )
   } else {
     return (
-      <ConnectButton>
+      <ConnectButton onClick={() => router.push('/feed')}>
         <Image src={visitorIcon} alt="Icone Visitante" width={32} height={32} />
         <span>Acessar como visitante</span>
       </ConnectButton>
--- src/pages/home/index.tsx
+++ src/pages/home/index.tsx
@@ -1,12 +1,20 @@
 import Image from 'next/image'
+import { useRouter } from 'next/router'
+
 import homeWelcomeImage from '../../assets/home_welcome_image.svg'
 import LoginButton from '@/components/LoginButton'
 import { Container, RigthSide, LeftSide, MenuLogin } from './styles'
+
 import { useSession } from 'next-auth/react'
 
 export default function Home() {
-  const { data } = useSession()
-  console.log(data)
+  const { status } = useSession()
+  const router = useRouter()
+
+  if (status === 'authenticated') {
+    router.push('/feed')
+  }
+
   return (
     <Container>
       <RigthSide>
@@ -14,14 +22,20 @@ export default function Home() {
       </RigthSide>
 
       <LeftSide>
-        <MenuLogin>
-          <h3>Boas vindas!</h3>
-          <span>Faça seu login ou acesse como visitante.</span>
+        {status === 'loading' ? ( // Adicionar algum tipo de informação visual para o loaging
+          <h2>Loading...</h2>
+        ) : (
+          status === 'unauthenticated' && (
+            <MenuLogin>
+              <h3>Boas vindas!</h3>
+              <span>Faça seu login ou acesse como visitante.</span>
 
-          <LoginButton variant="google" />
-          <LoginButton variant="github" />
-          <LoginButton />
-        </MenuLogin>
+              <LoginButton variant="google" />
+              <LoginButton variant="github" />
+              <LoginButton />
+            </MenuLogin>
+          )
+        )}
       </LeftSide>
     </Container>
   )
