aaaa37065d898a65f43e0a943fbc88eb60f471ca
Aron Adams <aronrapetto@hotmail.com>
2023-08-16 14:39:52 -0300

    fix: background of google icon

--- src/components/LoginButton/index.tsx
+++ src/components/LoginButton/index.tsx
@@ -1,5 +1,5 @@
 import Image from 'next/image'
-import googleLogo from '../../assets/google_logo.svg'
+import googleLogo from '../../assets/google_logo_1.png'
 import githubLogo from '../../assets/github_logo.svg'
 import visitorIcon from '../../assets/visitor_rocket_icon.svg'
 import { ConnectButton } from './styles'
@@ -12,21 +12,21 @@ export default function LoginButton({ variant }: LoginButtonProps) {
   if (variant === 'google') {
     return (
       <ConnectButton>
-        <Image src={googleLogo} alt="Icone Google" />
+        <Image src={googleLogo} alt="Icone Google" width={32} height={32} />
         <span>Entrar com Google</span>
       </ConnectButton>
     )
   } else if (variant === 'github') {
     return (
       <ConnectButton>
-        <Image src={githubLogo} alt="Icone GitHub" />
+        <Image src={githubLogo} alt="Icone GitHub" width={32} height={32} />
         <span>Entrar com Github</span>
       </ConnectButton>
     )
   } else {
     return (
       <ConnectButton>
-        <Image src={visitorIcon} alt="Icone Visitante" />
+        <Image src={visitorIcon} alt="Icone Visitante" width={32} height={32} />
         <span>Acessar como visitante</span>
       </ConnectButton>
     )
